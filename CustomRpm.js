897646b31f7a216115ce11f484212986ea6e0a52
HeinzGerhard <nicolascarlo@live.de>
2021-05-04 08:54:14 +0200

    AddCustomRPM

--- CustomRpm.js
+++ CustomRpm.js
@@ -1,41 +1,48 @@
 /* //////////////// Description ////////////////
 
-This Script sets the RPM of the Motor content usung a PID Controller
+This simple script allows performing endurance tests for extended periods of time. The user defines a test sequence consisting of arbitrary steps with arbitrary durations. The sequence can be repeated as many times as required. A data sample is recorded at the middle of every step in the test sequence.
 
+Assuming the user specified test is as follows:
+test = [[1200, 4], [1400, 2], [1200, 4], [1600, 1]];
 
-Usage:
+The test will be as follows: 
 
-q - Increase ki
-a - Decrease ki
-w - Increase kd
-s - Decrease kd
-e - Increase kp
-d - decrease kp
-r - Increase RPM
-f - Decrease RPM
-t - Increase Max Sum for Ki
-g - Decrease Max Sum for Ki
-0-9 - Set mark in Results File
+  . represents where a sample will be recorded
+  * represents the end of the script, or where the sequence will repeat
+
+     ^ Motor Input
+1600 |                         __._
+1400 |              __._      |    |               
+1200 |        __.__|    |__.__|    |  
+1000 |    ___|                     *
+     |_______0_____4____6_____10___11_________> Time
+
+In this example, the throttle starts at 1000 to initialize the ESC, and the sequence then begins. The sequence starts at time t=0 and finishes at time t=11. If the sequence repeats, it will repeat at 11s = 0s.
 
 ///////////// User defined script parameters //////////// */
 
-var minPWM = 1050;
-var maxPWM = 1950;
-// ESC initialization
-var minVal = 1000;  // ESC initialization value (us)
-var initDur = 2; // ESC initialization time (s)
-// StartupTime
-var startTime = 4;   //Startup Time
-var startPWM = 1150; // Startup Value
+// Test sequence: here we have four points defined, but you can 
+// put as many as you want. First value is ESC pwm, and second 
+// value is duration in seconds.
+var test = [[1000, 4], [1500, 2], [2000, 4], [2500, 1]];
 
 // Test repeat
-var maxTime = 100; // Not used
+var repeat = 1; // set to 1 to run the sequence only once
 
-var targetThrust = 1;
-var kpEntered = 10;   //Independent of timestep
-var kdEntered = 1;  //Independent of Timestep
-var kiEntered = 0.1; //Independent of timestep
-var maxI = 100000;
+// ESC initialization
+var minVal = 1000;  // ESC initialization value (us)
+var initDur = 4; // ESC initialization time (s)
+
+var filePrefix = "Polar";
+
+
+var targetRPM = 2500;
+var kpEntered = 0.1;   //Independent of timestep
+var kdEntered = 0.01;  //Independent of Timestep
+var kiEntered = 0.001; //Independent of timestep
+var maxI = 1000;
+var oldRPM = 0;
+var sum = 0;
 
 // TimeStep
 var time = 0.025; // Frequency
@@ -47,69 +54,67 @@ var kd = kdEntered; // No changem because DRPM different
 var ki = kiEntered*time; // Fix Kp so that it is constant over time
 
 // Init variables for Output
-var thrust = 0; // thrust Value
-var dif = 0;    // Difference between real and thrust
+var optical = 0; // Optical RPM Value
+var dif = 0;    // Difference between real and optical RPM
 var rpmChange = 0; // Change in RPM since last measurement
 var mark = 0;     // Mark in Output
+var step = 0;     // Mark in Output
 
-var filePrefix = "ConstVel";
 
+// UDP Values
 var receive_port = 55047; // the listening port on this PC
 var send_ip = "172.17.11.79"; // where to send the packet
 var send_port = 64126; // on which port to send the packet
 
+// runs the sequence
+var index = -1;
+var total = repeat;
+var totalTime = 0;
+var stepTime = 0;
+
 // start new log file
-var add = ["targetThrust", "Change","Integral","P","I","D","kp","ki","kd","Mark"];
+var add = ["TargetRPM", "Change","Integral","P","I","D","kp","ki","kd","Mark","Step"];
 rcb.files.newLogFile({prefix: filePrefix, additionalHeaders: add});
+
+///////////////// Beginning of the script //////////////////
+
+// ESC initialization
+rcb.console.print("Initializing ESC...");
+rcb.output.set("esc",minVal);
+rcb.wait(sequence, initDur);
 
 // hide console debug info
 rcb.console.setVerbose(false);
 
-///////////////// Beginning of the script //////////////////
-
-//Sequence Values
-var index = 0;
-var totalTime = 0;
-var oldThrust = 0;
-var sum = 0;
-var init = 1;
-readSensor(); // Get values during Startup
-initlization();
-
-function initlization(){
-// ESC initialization
-    rcb.udp.init(receive_port, send_ip, send_port, UDPInitialized);
-    rcb.console.print("Initializing ESC...");
-    rcb.output.set("escA",minVal);
-    rcb.wait(startup, initDur);
-}
-
-function startup(){
-// Motor Startup
-    rcb.console.print("Start Motor");
-    rcb.output.set("escA",startPWM);
-    rcb.wait(sequence, startTime);
-}
-
-
 function sequence(){
+    if(index > -1){
+        if(index === test.length){
+            // end of sequence
+            index = -1;
+            sequence();
+        }else{
+            // get step info
+            targetRPM = test[index][0];
+            var times = test[index][1];
 
-    init=0; // Disable logging in startup mode
-    index = index+time;
+            readSensors(); //Read Sensor values
 
-    //Clear console to avoid instabilities
-    if (index >=1){
-        rcb.console.clear();
+            if(stepTime > times){
+                index++
+                stepTime = 0
+            }
+
+            rcb.wait(sequence, time); //Wait until next execution
+        }
+    }else {
+        rcb.endScript();
     }
-
-    readSensors(); //Read Sensor values
-
-    rcb.wait(sequence, time); //Wait until next excetution
 }
 
 function readSensors(){
 
     totalTime = totalTime + time;
+    stepTime = stepTime + time;
 
     // take a sample
     rcb.sensors.read(setRPM, 1);
@@ -117,10 +122,10 @@ function readSensors(){
 
 function setRPM(result){
 
-    thrust = result.thrust.workingValue;
+    optical = result.motorOpticalSpeed.workingValue;
 
-    dif = targetThrust-thrust;
-    rpmChange = thrust - oldThrust;
+    dif = targetRPM-optical;
+    rpmChange = optical - oldRPM;
     sum = sum+dif;
 
     sum = Math.max(Math.min(sum,maxI),-maxI); // Limit sum
@@ -135,16 +140,16 @@ function setRPM(result){
     curPWM = Math.max(Math.min(curPWM + dif*kp + changeD+changeI,maxPWM ),minPWM); // Calculate new PWM value
 
     if (index>=1){ // Output once a second
-        rcb.console.print('PWM: '+ curPWM.toFixed(0)+ "\tdiff: " + dif.toFixed(0) + "\tCorr: "+ (dif*kp).toFixed(2) + '\tRPMChange: '+ rpmChange.toFixed(0) + "\tCorr: " + (kd*(-rpmChange)).toFixed(2) + "\toldThrust: " +oldThrust.toFixed(0)+ "\tSum: "+ sum.toFixed(0)+"Corr: " + (sum*ki).toFixed(2));
+        rcb.console.print('PWM: '+ curPWM.toFixed(0)+ "\tdiff: " + dif.toFixed(0) + "\tCorr: "+ (dif*kp).toFixed(2) + '\tRPMChange: '+ rpmChange.toFixed(0) + "\tCorr: " + (kd*(-rpmChange)).toFixed(2) + "\toldRpm: " +oldRPM.toFixed(0)+ "\tSum: "+ sum.toFixed(0)+"Corr: " + (sum*ki).toFixed(2));
         index = 0;
     }
 
     rcb.output.set("escA",curPWM); // Set PWM signal
 
-    oldThrust=thrust; //Save last value
+    oldRPM=optical; //Save last value
 
     // Save Log file
-    var add = [targetThrust, rpmChange,sum,dif*kp,sum*ki,kd*(-rpmChange),kp/time,ki/time,kd,mark];
+    var add = [targetRPM, rpmChange,sum,dif*kp,sum*ki,kd*(-rpmChange),kp/time,ki/time,kd,mark,stepTime];
     rcb.files.newLogEntry(result, readSensor,add);
 }
 
@@ -157,7 +162,7 @@ function readSensor(){
 }
 
 function saveResult(result){
-    var add = [targetThrust, rpmChange,sum,dif*kp,sum*ki,kd*(-rpmChange),kp/time,ki/time,kd,mark];
+    var add = [targetRPM, rpmChange,sum,dif*kp,sum*ki,kd*(-rpmChange),kp/time,ki/time,kd,mark];
     rcb.files.newLogEntry(result, readSensor,add);
 }
 
@@ -179,11 +184,11 @@ rcb.onKeyboardPress(function(key){
         kd = kd/1.1;
         rcb.console.print("New Kd: " +kd);
     } else if (key == 82){
-        targetThrust = targetThrust+1;
-        rcb.console.print("New targetThrust: " +targetThrust);
+        targetRPM = targetRPM+50;
+        rcb.console.print("New targetRPM: " +targetRPM);
     } else if (key == 70){
-        targetThrust = targetThrust-1;
-        rcb.console.print("New targetThrust: " +targetThrust);
+        targetRPM = targetRPM-50;
+        rcb.console.print("New targetRPM: " +targetRPM);
     } else if (key == 81){
         ki = ki*1.1;
         rcb.console.print("New ki: " +ki/time);
@@ -212,5 +217,3 @@ rcb.onKeyboardPress(function(key){
         rcb.console.print("You pressed " + ascii + " (ASCII " + key + ")");
     }
 });
-
-
